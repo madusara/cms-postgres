709078d1381fad0dced604865b13f5c98a2e2df0
Stephen Blom <stephen.blom@ascentic.se>
2024-06-07 13:41:57 +0530

    Update cors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,27 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default withPayload(nextConfig)
